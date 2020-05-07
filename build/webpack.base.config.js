77cdf45d78ea6dc1fea8e0ad5318601a41b8482c
lmxyjy <yjy15680489038@163.com>
2020-05-07 22:16:53 +0800

    init

---
+++ build/webpack.base.config.js
@@ -0,0 +1,27 @@
+const HtmlWebapckPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry:'./src/index.ts',
+    output:{
+        filename:"app.js"
+    },
+    resolve:{
+        extensions:['.js','.ts','.tsx']
+    },
+    module:{
+        rules:[
+            {
+                test:/\.tsx?$/i,
+                use:[{
+                    loader:'ts-loader'
+                }],
+                exclude:/node_modules/
+            }
+        ]
+    },
+    plugins:[
+        new  HtmlWebapckPlugin({
+            template:'./src/tpl/index.html'
+        })
+    ]
+}
